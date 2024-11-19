cb831a6f20366fc27031008ef8d4c8e4472ad8f3
Sasikumar Sahkana <102220196+Sharu2501@users.noreply.github.com>
2024-11-19 13:58:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,15 +94,14 @@ if st.sidebar.button("Afficher l'ACPM"):
     #st.session_state.fig = plot_metro(acpm_prim, stations, positions, titre="Arbre Couvrant de Poids Minimum (Prim)")
     st.write(f"Temps total de l'ACPM : {temps_formatte}")
 
-if "fig" in st.session_state:
-    st.plotly_chart(st.session_state.fig, use_container_width=True)
-
 if st.session_state.graphe_actif == "plus_court_chemin":
     st.session_state.fig = plot_metro( metro_graphe, stations, positions, chemin=chemin, titre=f"Plus Court Chemin : {temps_formatte}")
     st.plotly_chart(st.session_state.fig, use_container_width=True)
-else :
+else if :
     st.session_state.graphe_actif = "acpm"
     st.session_state.fig = plot_metro(acpm_prim, stations, positions, titre="Arbre Couvrant de Poids Minimum (Prim)")
+else :
+    st.plotly_chart(st.session_state.fig, use_container_width=True)
 
 # Affichage de la légende des lignes
 st.sidebar.subheader("Légende des lignes")
